c6e29a4a625af6ebea88d879396650ec135502dc
luis silva <luis.araujo@segsat.com>
2024-11-21 09:14:28 -0300

    feat: ✨ finish project

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -13,29 +13,70 @@ class Highlights extends StatelessWidget {
       child: CustomScrollView(
         slivers: <Widget>[
           const SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                "Destaques do dia",
-                style: TextStyle(fontFamily: "Caveat", fontSize: 32),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return HighlightItem(
-                  imageURI: items[index]["image"],
-                  itemTitle: items[index]["name"],
-                  itemPrice: items[index]["price"],
-                  itemDescription: items[index]["description"],
-                );
-              },
-              childCount: items.length,
-            ),
-          ),
+              child: Padding(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    "Destaques do dia",
+                    style: TextStyle(fontFamily: "Caveat", fontSize: 32),
+                    textAlign: TextAlign.center,
+                  ))),
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? _landscapeList()
+              : _portraitList()
         ],
+      ),
+    );
+  }
+}
+
+class _portraitList extends StatelessWidget {
+  const _portraitList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List items = destaques;
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+    );
+  }
+}
+
+class _landscapeList extends StatelessWidget {
+  const _landscapeList({Key? key}) : super(key: key);
+  final List items = destaques;
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return HighlightItem(
+                imageURI: items[index]["image"],
+                itemTitle: items[index]["name"],
+                itemPrice: items[index]["price"],
+                itemDescription: items[index]["description"],
+              );
+            },
+            childCount: items.length,
+          ),
+        );
+      }),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1,
       ),
     );
   }
